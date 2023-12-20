97ada3592d654a3a770883ccc630bb9b84a88aa3
Wylie Raditya <143018791+b1ll0boy@users.noreply.github.com>
2023-12-21 01:52:25 +0700

    Update diabetes_app.py

--- diabetes_app.py
+++ diabetes_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-from scikit-learn.preprocessing import StandardScaler
+from sklearn.preprocessing import scaler
 
 # Load models
 model_path = 'diabetes_models.sav'
