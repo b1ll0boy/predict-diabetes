7ecb31b66e988794f39615d23d70ec53c905b8e6
Wylie Raditya <143018791+b1ll0boy@users.noreply.github.com>
2023-12-21 02:41:37 +0700

    Update diabetes_app.py

--- diabetes_app.py
+++ diabetes_app.py
@@ -3,14 +3,14 @@ import pickle
 import numpy as np
 
 # Load models
-# model_path = 'diabetes_models.sav'
-# scaler_path = 'scaler.pkl'
-# try:
-diabetes_model = pickle.load(open('D:\Streamlit\project\diabetes_models.sav', 'rb'))
-scaler = pickle.load(open('D:\Streamlit\project\scaler.pkl', 'rb'))
-# except Exception as e:
-#     st.error(f"Error loading the model: {e}")
-#     st.stop()
+model_path = 'diabetes_models.sav'
+scaler_path = 'scaler.pkl'
+try:
+    diabetes_model = pickle.load(open(model_path, 'rb'))
+    scaler = pickle.load(open(scaler_path, 'rb'))
+except Exception as e:
+    st.error(f"Error loading the model: {e}")
+    st.stop()
 
 # Function to make predictions
 def predict_diabetes(inputs):
